24d2eaeb60474342e2c002ba049cb7a267cfbf2a
pranay2611 <pranaybiswas2611@gmail.com>
2025-11-10 00:29:31 +0530

    openapi definition update

--- src/main/java/com/mtech/accountservice/AccountserviceApplication.java
+++ src/main/java/com/mtech/accountservice/AccountserviceApplication.java
@@ -1,9 +1,18 @@
 package com.mtech.accountservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Account Service API",
+				description = "API for managing accounts in the banking system",
+				version = "1.0"
+		)
+)
 public class AccountserviceApplication {
 
 	public static void main(String[] args) {
